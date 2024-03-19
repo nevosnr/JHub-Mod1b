e7be45fccdbb4c1fff0aac12432a0a534b0d0b4d
Dave Neve <64417697+nevosnr@users.noreply.github.com>
2024-03-19 08:17:41 +0000

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  //this is a comment for testing pull requests
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
